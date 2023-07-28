a73c177d17b335c3c94dcc60149b321290858cb2
Giorgio . D <109972616+Giorgiod91@users.noreply.github.com>
2023-07-28 02:48:49 +0200

    Delete .history directory
    removed not needed files

--- .history/components/seasonalTitle_20230728003735.tsx
+++
@@ -1,3 +0,0 @@
-curl -X 'GET' \
-  'https://raider.io/api/v1/mythic-plus/season-cutoffs?season=season-df-2&region=eu' \
-  -H 'accept: application/json'
--- .history/components/seasonalTitle_20230728010704.tsx
+++
@@ -1,5 +0,0 @@
-'use client';
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-
-export default function SeasonalCutoff() {
